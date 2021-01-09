7c82f102dcb5714e2d118d3f19a3acf75ec529d2
IttetsuSato <74478789+IttetsuSato@users.noreply.github.com>
2021-01-09 15:53:09 +0900

    Update zoom.php

--- onder/zoom.php
+++ onder/zoom.php
@@ -4,9 +4,9 @@
   }
   
   
-  $zoom_url = 'https://api.zoom.us/v2/users/ittetsu51@docomo.ne.jp/meetings';
-  $zoom_api_key = "NfHBbpkZR_6gPoPitnri5g"; //ご自分のAPI Key
-  $zoom_api_secret = "94xeRcjqcGjFFGjSJYMHT3wmXhOAZixBnNcA"; //ご自分のAPI Secret
+  $zoom_url = '************';
+  $zoom_api_key = "***************"; //ご自分のAPI Key
+  $zoom_api_secret = "**********************:"; //ご自分のAPI Secret
   $expiration = time() + 30; //Tokenの有効期限（秒）
   
   $header = urlsafe_base64_encode('{"alg":"HS256","typ":"JWT"}');//署名アルゴリズムとトークンタイプ
